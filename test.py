9c8960c5de1c0302a7d85e1eb5d40bcd09955491
lentnerbalazs <61065537+lentnerbalazs@users.noreply.github.com>
2020-06-20 16:07:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,23 +5,27 @@ import subprocess
 import pandas as pd
 import numpy as np
 
+print("generate dataframe")
 df = pd.DataFrame(np.random.randint(0, 100, size=(100, 4)), columns=list("ABCD"))
 
+print("Writing out df")
 df.to_pickle("random_test.pkl")
 
-
+print("getting secret")
 key = os.environ["RAJK_KEY"]
 
 key_file_path = "temp_key"
 
+print("writing secret")
 with open(key_file_path, "w") as fp:
     fp.write(key)
 
+print("first subprocess")
 subprocess.call(["chmod", "600", key_file_path])
 
 
 data_file_path = "random_test.pkl"
-
+print("second subprocess")
 subprocess.call(
     [
         "scp",
